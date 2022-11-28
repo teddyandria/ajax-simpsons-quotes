510f7ed481a6b469886a65dc95b708b03165c637
teddyandria <teddy.andriamam@gmail.com>
2022-11-28 20:15:12 +0100

    some modif

--- script.js
+++ script.js
@@ -1,19 +1,22 @@
-// This function loads Simpsons data from the Simpsons API
-function fetchSimpsonJSON() {
-    // Feel free to download this HTML and edit it, to use another Simpson ID
-    const simpsonIndex = [0];
-    const url = fetch(`https://simpsons-quotes-api.herokuapp.com/quotes`)
+function fetchSimpsonsJSON() {
+    // Feel free to download this HTML and edit it, to use another Pokemon ID
+    const simpsonId = 1;
+    const url = `https://simpsons-quotes-api.herokuapp.com/quotes?count=${simpsonId}`;
+    axios.get(url)
         .then(function (response) {
-            return (response.json()); // response.data instead of response.json() with fetch
+            return response.data; // response.data instead of response.json() with fetch
         })
         .then(function (simpson) {
-            console.log(‘data decoded from JSON:‘, simpson);
+            console.log('data decoded from JSON:', simpson);
+
             // Build a block of HTML
-            const simpsonHtml = `
-      <h1><strong>${simpson[0].character}</strong></h1>
-      <img src=“${simpson[0].image}” />
-      <p>${simpson[0].quote}</p>
-      `;
-            document.querySelector(‘#simpson’).innerHTML = simpsonHtml;
+            const simpsonsHtml = `
+          <p><strong>${simpson[0].character}</strong></p>
+          <p>${simpson[0].quote}</p>
+          <img src="${simpson[0].image}"/>
+        `;
+            document.querySelector('#simpsons').innerHTML = simpsonsHtml;
         });
 }
+
+fetchSimpsonsJSON();
